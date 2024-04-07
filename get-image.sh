32436c97ead79eb51afe8c5739b0949d74930db5
Vincent Wang <ws_dev@163.com>
2024-04-07 12:06:26 +0800

    init

---
+++ get-image.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+programs=("curl" "bsdtar")
+for program in "${programs[@]}"
+do
+    if ! command -v "$program" &> /dev/null; then
+        echo "$program is not installed"
+        exit 1
+    fi
+done
+
+if [ ! -d "/tmp/arch" ]; then
+    mkdir /tmp/arch
+fi
+
+pushd /tmp/arch
+curl -# -O https://mirrors.edge.kernel.org/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.gz
+bsdtar -xpf archlinux-bootstrap-x86_64.tar.gz
+
+bsdtar -cpf archlinux-bootstrap.tar -C root.x86_64 .
+popd
+
+if [ ! -d "/mnt/c/arch-image" ]; then
+    mkdir /mnt/c/arch-image
+fi
+
+mv /tmp/arch/archlinux-bootstrap.tar /mnt/c/arch-image
+rm -rf /tmp/arch
---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+# locale
+sed -i \
+-e "s/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" \
+/etc/locale.gen
+
+locale-gen
+
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+
+# pacman
+echo 'Server = https://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+pacman-key --init && pacman-key --populate
+pacman -Sy --noconfirm archlinux-keyring && pacman -Su --noconfirm
+
+# uninstall packages
+pacman -R --noconfirm arch-install-scripts
+
+# install packages
+pacman -S --needed --noconfirm vim sudo wget man base-devel pacman-contrib
+
+# create user
+read -p "Please enter your username: " username
+sed -i -e "s/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/" /etc/sudoers
+useradd $username -m -G wheel -s /bin/bash
+echo "password for $username"
+passwd $username
+
+# default user, enable systemd
+echo -e "[user]\ndefault=$username\n[boot]\nsystemd=true" > /etc/wsl.conf
+
+# docker
+pacman -S --needed --noconfirm docker
+usermod -aG docker $username
+
+mkdir /etc/docker
+
+cat << EOF > /etc/docker/daemon.json
+{
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  }
+}
+EOF
